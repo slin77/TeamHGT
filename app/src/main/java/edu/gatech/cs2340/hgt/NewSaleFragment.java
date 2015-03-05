31506ad169554532e3dff1e29d00509dc447932a
jinghong <jhong84@gatech.edu>
2015-03-04 22:46:53 -0500

    add_sale_fragment

--- app/src/main/java/edu/gatech/cs2340/hgt/NewSaleFragment.java
+++ app/src/main/java/edu/gatech/cs2340/hgt/NewSaleFragment.java
@@ -74,6 +74,7 @@ public class NewSaleFragment extends Fragment {
                 status.setText("price field can not be empty");
                 return;
             }
+            // update db
             activity.returnToUserHomeActivity();
         }
     }
@@ -89,16 +90,6 @@ public class NewSaleFragment extends Fragment {
         super.onDetach();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface Back {
         public boolean returnToUserHomeActivity();
     }
