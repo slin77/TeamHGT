c2e23a3bacc473cfdc462fe1abb32fc806a19489
jinghong <jhong84@gatech.edu>
2015-04-03 13:56:11 -0400

    revised test

--- app/src/androidTest/java/edu/gatech/cs2340/hgt/checkPasswordMatchTest.java
+++ app/src/androidTest/java/edu/gatech/cs2340/hgt/checkPasswordMatchTest.java
@@ -22,15 +22,15 @@ public class checkPasswordMatchTest extends ActivityInstrumentationTestCase2<Sig
     // with the valid length
     public void testUnmatchedPassword() {
 
-        // unmatched number
-        boolean test1 = UserService.checkPasswordMatch("password123", "password124");
+        // unmatched length
+        boolean test1 = UserService.checkPasswordMatch("password123", "ppassword123");
         assertFalse(test1);
 
-        // unmatched length
-        boolean test2 = UserService.checkPasswordMatch("password123", "ppassword123");
+        boolean test2 = UserService.checkPasswordMatch("ppassword123", "password123");
         assertFalse(test2);
 
-        boolean test3 = UserService.checkPasswordMatch("ppassword123", "password123");
+        // unmatched number
+        boolean test3 = UserService.checkPasswordMatch("password123", "password124");
         assertFalse(test3);
 
         // unmatched letter (case sensitive)
@@ -40,18 +40,22 @@ public class checkPasswordMatchTest extends ActivityInstrumentationTestCase2<Sig
         // unmatched letter
         boolean test5 = UserService.checkPasswordMatch("123password", "123passward");
         assertFalse(test5);
+
+        // unmatched letter/number
+        boolean test6 = UserService.checkPasswordMatch("123password", "12password3");
+        assertFalse(test6);
     }
 
     public void testMatchedPassword() {
 
-        boolean test6 = UserService.checkPasswordMatch("password123", "password123");
-        assertTrue(test6);
+        boolean test1 = UserService.checkPasswordMatch("password123", "password123");
+        assertTrue(test1);
 
-        boolean test7 = UserService.checkPasswordMatch("1234password", "1234password");
-        assertTrue(test7);
+        boolean test2 = UserService.checkPasswordMatch("1234password", "1234password");
+        assertTrue(test2);
 
-        boolean test8 = UserService.checkPasswordMatch("pass12word", "pass12word");
-        assertTrue(test8);
+        boolean test3 = UserService.checkPasswordMatch("pass12word", "pass12word");
+        assertTrue(test3);
     }
 
 }
